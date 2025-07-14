189753b3480aa712150b2cf95668b9e497155983
Shreyas Malhotra <126407864+shreyas-malhotra@users.noreply.github.com>
2025-07-15 00:01:07 +0530

    Update get_rekcah.ps1

--- get_rekcah.ps1
+++ get_rekcah.ps1
@@ -2,7 +2,7 @@ Write-Host "Note: This script was last updated at 15/07/2025 and may be out of s
 
 $dir_name = "REKCAH!"
 New-Item -ItemType Directory -Path $dir_name -Force | Out-Null
-cd $dir_name
+Set-Location $dir_name
 
-Invoke-WebRequest https://www.blackhillsinfosec.com/wp-content/uploads/2023/06/BvB_LOG4J_FINISHED_05122023.pdf -OutFile "REKCAH: Bear vs Bear"
-Invoke-WebRequest https://www.blackhillsinfosec.com/wp-content/uploads/2022/02/Zine_FINAL_all_pages.pdf -OutFile "REKCAH: The Future Is ****** - Ashcan Edition Preview"
+Invoke-WebRequest https://www.blackhillsinfosec.com/wp-content/uploads/2023/06/BvB_LOG4J_FINISHED_05122023.pdf -OutFile "REKCAH - Bear vs Bear"
+Invoke-WebRequest https://www.blackhillsinfosec.com/wp-content/uploads/2022/02/Zine_FINAL_all_pages.pdf -OutFile "REKCAH - The Future Is ****** - Ashcan Edition Preview"
