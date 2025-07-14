2b3e5c69bba37568f743c9d95f526c062d3bc78c
Shreyas Malhotra <126407864+shreyas-malhotra@users.noreply.github.com>
2025-07-15 01:22:45 +0530

    Update get_rekcah.ps1

--- get_rekcah.ps1
+++ get_rekcah.ps1
@@ -5,4 +5,4 @@ New-Item -ItemType Directory -Path $dir_name -Force | Out-Null
 Set-Location $dir_name
 
 Invoke-WebRequest https://www.blackhillsinfosec.com/wp-content/uploads/2023/06/BvB_LOG4J_FINISHED_05122023.pdf -OutFile "REKCAH - Bear vs Bear"
-Invoke-WebRequest https://www.blackhillsinfosec.com/wp-content/uploads/2022/02/Zine_FINAL_all_pages.pdf -OutFile "REKCAH - The Future Is ****** - Ashcan Edition Preview"
+Invoke-WebRequest https://www.blackhillsinfosec.com/wp-content/uploads/2022/02/Zine_FINAL_all_pages.pdf -OutFile "REKCAH - The Future Is Fucked - Ashcan Edition Preview"
